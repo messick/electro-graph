1138d14d2fa58f7371ff493d31246a8578c853fd
Nick Messick <nmessick@gmail.com>
2012-08-05 13:22:59 -0700

    generate rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ElectroGraph::Application.config.secret_token = '312d2663263ac20ac5e478957d7497a539ddbaadca4f34d6ce7f1918dee2a8f7ff496b94c5cb29a6ad58f0244ce9f731a8881bda52aa851a5ed9ccc033da50a3'
